d8ac0a1cdd21f20551e2dd1da560b42b585876d2
Habert <habertotieno4@gmail.com>
2025-07-31 20:17:52 +0300

    ajali-backend

--- server/routes/base_routes.py
+++ server/routes/base_routes.py
@@ -4,5 +4,11 @@ from flask import Blueprint, jsonify
 base = Blueprint('base', __name__)
 
 @base.route("/")
-def index():
-    return jsonify({"message": "Ajali backend is live!"}), 200
+
+def home():
+    return "Hello from Flask + Waitress!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
